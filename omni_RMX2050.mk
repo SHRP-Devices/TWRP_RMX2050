2a4fa0164e3f6daeea7bd02ecd643d4073e38159
siddhesh <skoyande17@gmail.com>
2021-03-21 20:15:48 +0530

    Init

---
+++ omni_RMX2050.mk
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2020 The Android Open Source Project
+# Copyright (C) 2020 The TWRP Open Source Project
+# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from phoenix device
+$(call inherit-product, device/realme/RMX2050/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := RMX2050
+PRODUCT_NAME := omni_RMX2050
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := narzo 20a
+PRODUCT_MANUFACTURER := realme
+
+# Dimen
+TARGET_SCREEN_HEIGHT := 1600
+TARGET_SCREEN_WIDTH := 720
